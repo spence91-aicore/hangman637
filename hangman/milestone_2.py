dd330ae9e7321acb676be840034ec44a6fc6cb81
Spencer Ringhoj At Home <spencer.ringhoj@home.uk>
2024-05-14 21:28:35 +0100

    task1 + list of possible words

---
+++ hangman/milestone_2.py
@@ -0,0 +1,13 @@
+import typing
+from typing import List
+
+word_list : List[str] = \
+    ['pineapple',
+     'apple',
+     'blueberry',
+     'raspberry',
+     'kiwi'
+    ]
+
+if __name__ == "__main__":
+    print(word_list)
