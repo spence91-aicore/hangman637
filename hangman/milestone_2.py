3e3ec170429c50319f9bbcb7c1b2a631a0050f92
Spencer Ringhoj At Home <spencer.ringhoj@home.uk>
2024-05-14 21:33:42 +0100

    task2 choose a random word from the list

--- hangman/milestone_2.py
+++ hangman/milestone_2.py
@@ -1,5 +1,7 @@
+import random
 import typing
 from typing import List
+
 
 word_list : List[str] = \
     ['pineapple',
@@ -9,5 +11,11 @@ word_list : List[str] = \
      'kiwi'
     ]
 
+word = random.choice(word_list)
+
 if __name__ == "__main__":
+    #print word list
     print(word_list)
+    #print random word
+    # random.choice(word_list)
+    print(word)
